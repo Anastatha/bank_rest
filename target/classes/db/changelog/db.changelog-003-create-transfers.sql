e7689927dc14b594c53a0530b07b89a7e1bf99e8
RodionovaAP <rodionovaap@admlr.lipetsk.ru>
2025-09-30 11:40:04 +0300

    Remove IDE and build files from Git tracking

--- target/classes/db/changelog/db.changelog-003-create-transfers.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE transfers (
-                           id SERIAL PRIMARY KEY,
-                           from_card_id BIGINT NOT NULL,
-                           to_card_id BIGINT NOT NULL,
-                           amount DECIMAL(19,2) NOT NULL,
-                           transfer_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                           CONSTRAINT fk_transfers_from_cards FOREIGN KEY (from_card_id) REFERENCES cards(id),
-                           CONSTRAINT fk_transfers_to_cards FOREIGN KEY (to_card_id) REFERENCES cards(id)
-);
--- target/classes/db/changelog/db.changelog-004-add-block_requested-cards.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE cards ADD COLUMN block_requested BOOLEAN DEFAULT FALSE;
